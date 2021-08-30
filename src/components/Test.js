5ce1d28b3638ef3437bc2a68454968168590d838
croissant93 <croissant9393@gmail.com>
2021-08-30 14:55:10 +0900

    Project 초기세팅 - 2

---
+++ src/components/Test.js
@@ -0,0 +1,5 @@
+function Test() {
+  return <div className="App">아이아니</div>;
+}
+
+export default Test;
---
+++ src/pages/Test2.js
@@ -0,0 +1,5 @@
+function Test2() {
+  return <div className="App">아이아니</div>;
+}
+
+export default Test2;
